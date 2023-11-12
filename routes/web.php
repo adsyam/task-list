f8a6d00d660527e05c1476dcbeea71a12f0494ee
adsyam <yambaoadrianne@gmail.com>
2023-11-13 01:46:15 +0800

    CHANGE: delete route

--- routes/web.php
+++ routes/web.php
@@ -51,13 +51,20 @@ Route::post('/tasks', function (TaskRequest $request) {
 })->name('tasks.store');
 
 Route::put('/tasks/{task}', function (Task $task, TaskRequest $request) {
-    $task->update($request->validated()); //update() does the same thing as create() but it operates in an already existing model"
+    $task->update($request->validated());
+    //update() does the same thing as create() but it operates in an already existing model"
 
     return redirect()->route('tasks.show', ['task' => $task->id])
         ->with('success', 'Task updated successfully!');
 
     // with() lets you set some session data
 })->name('tasks.update');
+
+Route::delete('/tasks/{task}', function (Task $task) {
+    $task->delete(); // delete() method removes records from the db
+
+    return redirect()->route('tasks.index')->with('success', 'Task deleted successfully!');
+})->name('tasks.destroy');
 
 Route::fallback(function () {
     return 'Still got somewhere!';
